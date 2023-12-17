eb181379156e035579dad01b8ec03feb7c183b33
LuisChore <luis.vazquez.ch@hotmail.com>
2023-12-17 10:04:33 -0600

    Model Class

--- 01MovieApp/app/build.gradle.kts
+++ 01MovieApp/app/build.gradle.kts
@@ -41,4 +41,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //Retrofit library
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    //GSON converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
